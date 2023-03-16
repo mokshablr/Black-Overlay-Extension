af2a0fab64ced801b870444c668146f8fbbad763
Mokshablr <arjunrmaniyani@gmail.com>
2023-03-16 21:54:31 +0530

    Connected pop.js to content.js. Slider controls Black-Overlay

--- content.js
+++ content.js
@@ -8,5 +8,14 @@ init();
 
 function on() {
     document.getElementById("Black-Overlay").style.display = "block";
+    document.getElementById("Black-Overlay").style.opacity = 0.5;
 }
 on();
+
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message){
+  console.log(message.value);
+  document.getElementById("Black-Overlay").style.opacity = message.value;
+}
--- pop.js
+++ pop.js
@@ -5,6 +5,19 @@ output.innerHTML = slider.value; // Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
-}
+  let val = this.value;
+  let params = {
+                active: true,
+                currentWindow: true
+            }
+  chrome.tabs.query(params, gotTabs);
 
-document.getElementById("Black-Overlay").style.opacity=slider.value;
+  function gotTabs(tabs){
+
+    let msg = {
+        value : val
+    }
+
+    chrome.tabs.sendMessage(tabId = tabs[0].id, message = msg);
+  }
+}
